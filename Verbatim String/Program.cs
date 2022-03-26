eba2ab0839c2584940039f2d66f118edae5091e1
Stephen Moody <stephenmoody87@gmail.com>
2022-03-26 11:08:58 -0400

    Add project files.

---
+++ Verbatim String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Verbatim_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+            Console.WriteLine("Invoice: 1021\t\tComplete!");
+            Console.WriteLine("Invoice: 1022\t\tComplete!");
+            Console.WriteLine("\nOutput Directory:\t");
+            Console.WriteLine(@"c:\invoices");
+
+            //Japanese invoice
+            Console.WriteLine("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            Console.WriteLine(@"c:\invoices\apps.exe -j");
+        }
+    }
+}
